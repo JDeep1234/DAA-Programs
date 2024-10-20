27b9963e9449442b84cbd0b161f54addefc46234
JDeep1234 <132117873+JDeep1234@users.noreply.github.com>
2024-10-20 13:22:30 +0530

    Update floydsAlgo.c

--- floydsAlgo.c
+++ floydsAlgo.c
@@ -2,39 +2,71 @@
 #include <stdlib.h>
 #define INF 9999
 
-int min(int a, int b) {
+int min(int a, int b)
+{
     return (a < b) ? a : b;
 }
+void floyd(int **graph, int n)
+{
+    int i, j, k;
 
-void floyd(int graph[][100], int n) {
-    for (int k = 0; k < n; k++)
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
+    for (k = 0; k < n; k++)
+    {
+        for (i = 0; i < n; i++)
+        {
+            for (j = 0; j < n; j++)
+            {
+
                 graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j]);
+            }
+        }
+    }
 }
 
-int main() {
-    int n, graph[100][100];
+int main()
+{
+    int n, i, j;
 
-    printf("Enter number of vertices: ");
+    printf("Enter the number of vertices: ");
     scanf("%d", &n);
 
-    printf("Enter adjacency matrix (use -1 for no path):\n");
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++) {
+    int **graph = (int **)malloc(n * sizeof(int *));
+    for (i = 0; i < n; i++)
+    {
+        graph[i] = (int *)malloc(n * sizeof(int));
+    }
+
+    printf("Enter the adjacency matrix:\n");
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
             scanf("%d", &graph[i][j]);
             if (i != j && graph[i][j] == -1)
                 graph[i][j] = INF;
         }
+    }
 
     floyd(graph, n);
 
-    printf("Shortest distances between vertices:\n");
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++)
-            printf("%s\t", (graph[i][j] == INF) ? "INF" : "%d", graph[i][j]);
+    printf("Shortest distances between every pair of vertices:\n");
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            if (graph[i][j] == INF)
+                printf("INF\t");
+            else
+                printf("%d\t", graph[i][j]);
+        }
         printf("\n");
     }
+
+    for (i = 0; i < n; i++)
+    {
+        free(graph[i]);
+    }
+    free(graph);
 
     return 0;
 }
